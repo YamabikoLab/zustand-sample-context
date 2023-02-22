30c500076b11cef3034541a2a51d5c4326b86e32
yamabiko <yhijikata@systemlancer.com>
2023-02-22 19:46:36 +0000

    リファクタリング

--- src/store/useCounterStore.tsx
+++ src/store/useCounterStore.tsx
@@ -1,4 +1,6 @@
-import { create } from "zustand";
+import React from "react";
+import { createContext, type ReactNode, useContext } from "react";
+import { createStore, type StoreApi, useStore } from "zustand";
 
 interface CounterState {
   count: number;
@@ -11,14 +13,36 @@ interface CounterAction {
 
 export type CounterStore = CounterState & CounterAction;
 
-const useCounterStore = create<CounterStore>((set) => ({
-  count: 0,
-  increase: () => {
-    set((state) => ({ count: state.count + 1 }));
-  },
-  decrease: () => {
-    set((state) => ({ count: state.count - 1 }));
-  },
-}));
+const createCounterStore = (): StoreApi<CounterStore> =>
+  createStore<CounterStore>((set) => ({
+    count: 0,
+    increase: () => {
+      set((state) => ({ count: state.count + 1 }));
+    },
+    decrease: () => {
+      set((state) => ({ count: state.count - 1 }));
+    },
+  }));
 
-export default useCounterStore;
+const CounterContext = createContext<ReturnType<
+  typeof createCounterStore
+> | null>(null);
+
+export const CounterProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}): JSX.Element => {
+  const store = createCounterStore();
+  return (
+    <CounterContext.Provider value={store}>{children}</CounterContext.Provider>
+  );
+};
+
+export const useCounterStore = (): CounterStore => {
+  const store = useContext(CounterContext);
+  if (store === null) {
+    throw new Error("no provider");
+  }
+  return useStore(store);
+};
